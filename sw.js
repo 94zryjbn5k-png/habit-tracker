d804b9bbc6945cd32f1c3445c0aff8e31d49cc4d
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-08-02 14:20:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v12';
+const CACHE = 'habit-tracker-v13';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
