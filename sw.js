9db773273b5e7ef28f9ba8b1c2c408509e5c6bab
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-16 15:32:34 +0200

    zadani od trenera + kolonka pro kazdou serii

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v46';
+const CACHE = 'habit-tracker-v47';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
