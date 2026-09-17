41c5806f063127f30aefbeac4677d78e82920a0e
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-17 03:21:36 +0200

    cviky bez vah - pole k odskrtnuti

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v57';
+const CACHE = 'habit-tracker-v58';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
