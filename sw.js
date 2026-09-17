10fe5ce6654690a9b3aca01b33e5b79562016164
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-17 10:15:47 +0200

    povoleny hlubsi cesty - doplnkove cviky jdou odskrtnout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v58';
+const CACHE = 'habit-tracker-v59';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
