3d9be4d54b6e70d0b3c280dc7e5e36dbbbd4fcfd
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-18 19:46:21 +0200

    v nedeli nahled pristiho tydne

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v67';
+const CACHE = 'habit-tracker-v68';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
