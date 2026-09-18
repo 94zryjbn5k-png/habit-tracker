0e6eb466a4e869723ea169e1a8902c41c234d48e
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-18 19:36:03 +0200

    vyber dne jako tydenni mrizka

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v64';
+const CACHE = 'habit-tracker-v65';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
