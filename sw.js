ad754425580f7ac80ccf1ec510282bc1df59f49d
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-19 01:05:04 +0200

    poznamka ke cviku a historie vykonu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v70';
+const CACHE = 'habit-tracker-v71';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
