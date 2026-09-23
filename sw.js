5651a22ffc688be08696539b0451aa4b57d32127
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-23 14:27:12 +0200

    graf: opakovani se lamou jen za lomitkem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v78';
+const CACHE = 'habit-tracker-v79';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
