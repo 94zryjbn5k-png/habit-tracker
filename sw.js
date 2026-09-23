36633a29157643f2e81bf7907aee6dd3248fc619
94zryjbn5k-png <94zryjbn5k@privaterelay.appleid.com>
2026-09-23 14:21:34 +0200

    graf: sloupce zarovnane, kompaktni opakovani

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'habit-tracker-v77';
+const CACHE = 'habit-tracker-v78';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
